254130508a0657e455c5845831b495f1752c1856
ZSte <ZSte@users.noreply.github.com>
2016-12-03 17:18:22 +0100

    Changed the Navigation Drawer Menu to show the right options

--- app/src/main/java/de/mobilityhacks/ydda/youdontdrivealone/MainActivity.java
+++ app/src/main/java/de/mobilityhacks/ydda/youdontdrivealone/MainActivity.java
@@ -97,20 +97,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.friendsIt) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.rankingIt) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.questsIt) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;

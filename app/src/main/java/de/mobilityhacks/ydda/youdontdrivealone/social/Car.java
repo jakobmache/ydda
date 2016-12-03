d0cd5548666e80d289ae4174b9b3f0faf32f7f6d
SaikoSan <nicole.0810@web.de>
2016-12-03 23:40:12 +0100

    Add files via upload
    Added Data for simulation it contains some cars.
    Attributes of car added / changed.

--- app/src/main/java/de/mobilityhacks/ydda/youdontdrivealone/social/Car.java
+++ app/src/main/java/de/mobilityhacks/ydda/youdontdrivealone/social/Car.java
@@ -1,9 +1,27 @@
-package de.mobilityhacks.ydda.youdontdrivealone.social;
+package de.mobilityhacks.ydda.youdontdrivealone;
 
 //representing the cars that can be rented
 public class Car {
-	String name;
 	String brand;
+	String name;
 	//TODO Kriterien und so..
-	double ecofrendliness;
+	double consumptionPerKm;
+	String fuel;
+	double distanceTravelled;
+	double fuelUsed;
+	
+	public Car(String brand, String name, double consumptionPerKm, String fuel) {
+		this.brand = brand;
+		this.name = name;
+		this.consumptionPerKm = consumptionPerKm;
+		this.fuel = fuel;
+	}
+	
+	//because of lack of a real source we just simulate a drive
+	//you can imagine that the real values are tracked and the calculation
+	//can happen in this method instead
+	public void drive(double distanceTravelled, double fuelUsed) {
+		this.distanceTravelled = distanceTravelled;
+		this.fuelUsed = fuelUsed;
+	}
 }
